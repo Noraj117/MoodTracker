8018d467ad98c66f61d49439d12b2aeb00ef17e9
Noraj117 <alexandre.collet.fra@gmail.com>
2019-02-20 17:36:38 +0100

    Implement gesture detector method

--- app/src/main/java/com/collet/alexandre/moodtracker/controller/MainActivity.java
+++ app/src/main/java/com/collet/alexandre/moodtracker/controller/MainActivity.java
@@ -4,9 +4,12 @@ import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.collet.alexandre.moodtracker.R;
 
@@ -14,22 +17,54 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageButton mCommentBtn;
     private ImageButton mHistoryBtn;
+    private ImageView mImageView;
 
+    private SwipeGestureDetector mGestureDetector;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mGestureDetector = new SwipeGestureDetector(this);
+
         mCommentBtn = (ImageButton) findViewById(R.id.main_activity_comment);
         mHistoryBtn = (ImageButton) findViewById(R.id.main_activity_history);
 
+        mImageView = (ImageView) findViewById(R.id.main_activity_img);
+        mImageView.setImageResource(R.drawable.smiley_happy);
+
         mCommentBtn.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 addComment();
             }
         });
 
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        return mGestureDetector.onTouchEvent(ev);
+    }
+
+    public void onSwipe(SwipeGestureDetector.SwipeDirection direction) {
+        String message = "";
+        switch(direction) {
+            case LEFT_TO_RIGHT:
+                message = "Left to right swipe";
+                break;
+            case RIGHT_TO_LEFT:
+                message = "Right to left swipe";
+                break;
+            case TOP_TO_BOTTOM:
+                message = "Top to bottom swipe";
+                break;
+            case BOTTOM_TO_TOP:
+                message = "Bottom to top swipe";
+                break;
+        }
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
     private void addComment() {
